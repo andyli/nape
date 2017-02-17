df13962cf1929576a73cf6de38c8b2dd29904f43
Andy Li <andy@onthewings.net>
2017-02-17 16:24:43 +0800

    added the Dummy* test files generated from caxe, init travisci config

---
+++ test/DummyMemory.hx
@@ -0,0 +1,275 @@
+package ;
+import DummyJSMain;
+import DummyNapeMain;
+import zpp_nape.Const;
+import zpp_nape.ID;
+import zpp_nape.util.Array2;
+import zpp_nape.util.Circular;
+import zpp_nape.util.DisjointSetForest;
+import zpp_nape.util.FastHash;
+import zpp_nape.util.Debug;
+import zpp_nape.util.Flags;
+import zpp_nape.util.Math;
+import zpp_nape.util.Names;
+import zpp_nape.util.Pool;
+import zpp_nape.util.Lists;
+import zpp_nape.util.Queue;
+import zpp_nape.util.UserData;
+import zpp_nape.util.RBTree;
+import zpp_nape.util.WrapLists;
+import zpp_nape.space.Broadphase;
+import zpp_nape.space.DynAABBPhase;
+import zpp_nape.space.SweepPhase;
+import zpp_nape.shape.Circle;
+import zpp_nape.shape.Edge;
+import zpp_nape.shape.Polygon;
+import zpp_nape.shape.Shape;
+import zpp_nape.phys.Body;
+import zpp_nape.phys.Compound;
+import zpp_nape.phys.FeatureMix;
+import zpp_nape.phys.FluidProperties;
+import zpp_nape.phys.Interactor;
+import zpp_nape.phys.Material;
+import zpp_nape.geom.AABB;
+import zpp_nape.space.Space;
+import zpp_nape.geom.Collide;
+import zpp_nape.geom.Convex;
+import zpp_nape.geom.ConvexRayResult;
+import zpp_nape.geom.Geom;
+import zpp_nape.geom.Cutter;
+import zpp_nape.geom.GeomPoly;
+import zpp_nape.geom.Mat23;
+import zpp_nape.geom.MarchingSquares;
+import zpp_nape.geom.MatMN;
+import zpp_nape.geom.MatMath;
+import zpp_nape.geom.Monotone;
+import zpp_nape.geom.PartitionedPoly;
+import zpp_nape.geom.PolyIter;
+import zpp_nape.geom.Simple;
+import zpp_nape.geom.Ray;
+import zpp_nape.geom.Simplify;
+import zpp_nape.geom.SweepDistance;
+import zpp_nape.geom.Triangular;
+import zpp_nape.geom.Vec2;
+import zpp_nape.geom.Vec3;
+import zpp_nape.geom.VecMath;
+import zpp_nape.dynamics.Contact;
+import zpp_nape.dynamics.InteractionFilter;
+import zpp_nape.dynamics.InteractionGroup;
+import zpp_nape.dynamics.SpaceArbiterList;
+import zpp_nape.constraint.AngleJoint;
+import zpp_nape.constraint.Constraint;
+import zpp_nape.constraint.DistanceJoint;
+import zpp_nape.constraint.LinearJoint;
+import zpp_nape.dynamics.Arbiter;
+import zpp_nape.constraint.LineJoint;
+import zpp_nape.constraint.MotorJoint;
+import zpp_nape.constraint.PivotJoint;
+import zpp_nape.constraint.UserConstraint;
+import zpp_nape.constraint.PulleyJoint;
+import zpp_nape.constraint.WeldJoint;
+import zpp_nape.callbacks.Callback;
+import zpp_nape.callbacks.CbSet;
+import zpp_nape.callbacks.CbType;
+import zpp_nape.callbacks.CbSetPair;
+import zpp_nape.callbacks.OptionType;
+import nape.Config;
+import nape.TArray;
+import zpp_nape.callbacks.Listener;
+import nape.util.Debug;
+import nape.util.BitmapDebug;
+import nape.space.Broadphase;
+import nape.util.ShapeDebug;
+import nape.shape.Circle;
+import nape.shape.Edge;
+import nape.shape.EdgeIterator;
+import nape.shape.EdgeList;
+import nape.shape.Polygon;
+import nape.shape.Shape;
+import nape.shape.ShapeIterator;
+import nape.shape.ShapeList;
+import nape.shape.ShapeType;
+import nape.shape.ValidationResult;
+import nape.space.Space;
+import nape.phys.BodyIterator;
+import nape.phys.BodyList;
+import nape.phys.BodyType;
+import nape.phys.Compound;
+import nape.phys.CompoundIterator;
+import nape.phys.CompoundList;
+import nape.phys.FluidProperties;
+import nape.phys.GravMassMode;
+import nape.phys.InertiaMode;
+import nape.phys.Interactor;
+import nape.phys.InteractorIterator;
+import nape.phys.InteractorList;
+import nape.phys.MassMode;
+import nape.phys.Material;
+import nape.geom.AABB;
+import nape.phys.Body;
+import nape.geom.ConvexResultIterator;
+import nape.geom.ConvexResult;
+import nape.geom.ConvexResultList;
+import nape.geom.Geom;
+import nape.geom.GeomPolyIterator;
+import nape.geom.GeomPolyList;
+import nape.geom.GeomVertexIterator;
+import nape.geom.IsoFunction;
+import nape.geom.MarchingSquares;
+import nape.geom.GeomPoly;
+import nape.geom.MatMN;
+import nape.geom.Mat23;
+import nape.geom.Ray;
+import nape.geom.RayResultIterator;
+import nape.geom.RayResultList;
+import nape.geom.RayResult;
+import nape.geom.Vec2Iterator;
+import nape.geom.Vec2List;
+import nape.geom.Vec2;
+import nape.geom.Winding;
+import nape.dynamics.Arbiter;
+import nape.geom.Vec3;
+import nape.dynamics.ArbiterIterator;
+import nape.dynamics.ArbiterType;
+import nape.dynamics.ArbiterList;
+import nape.dynamics.CollisionArbiter;
+import nape.dynamics.ContactIterator;
+import nape.dynamics.ContactList;
+import nape.dynamics.Contact;
+import nape.dynamics.InteractionFilter;
+import nape.dynamics.FluidArbiter;
+import nape.dynamics.InteractionGroupIterator;
+import nape.dynamics.InteractionGroup;
+import nape.dynamics.InteractionGroupList;
+import nape.constraint.AngleJoint;
+import nape.constraint.ConstraintIterator;
+import nape.constraint.ConstraintList;
+import nape.constraint.DistanceJoint;
+import nape.constraint.LinearJoint;
+import nape.constraint.Constraint;
+import nape.constraint.MotorJoint;
+import nape.constraint.PivotJoint;
+import nape.constraint.LineJoint;
+import nape.constraint.PulleyJoint;
+import nape.constraint.UserConstraint;
+import nape.constraint.WeldJoint;
+import nape.callbacks.BodyCallback;
+import nape.callbacks.BodyListener;
+import nape.callbacks.CbEvent;
+import nape.callbacks.CbType;
+import nape.callbacks.CbTypeIterator;
+import nape.callbacks.CbTypeList;
+import nape.callbacks.ConstraintCallback;
+import nape.callbacks.ConstraintListener;
+import nape.callbacks.InteractionCallback;
+import nape.callbacks.Callback;
+import nape.callbacks.InteractionType;
+import nape.callbacks.Listener;
+import nape.callbacks.ListenerIterator;
+import nape.callbacks.ListenerList;
+import nape.callbacks.InteractionListener;
+import nape.callbacks.ListenerType;
+import nape.callbacks.PreCallback;
+import nape.callbacks.OptionType;
+import nape.callbacks.PreFlag;
+import nape.callbacks.PreListener;
+#if nape_swc@:keep #end
+class Reference{
+    public var name:String;
+    public var obj:TObject;
+    public function new(name:String,obj:TObject){
+        this.name=name;
+        this.obj=obj;
+    }
+}
+#if nape_swc@:keep #end
+class PushResult{
+    public var isnew:Bool;
+    public var obj:TObject;
+    public function new(isnew:Bool,obj:TObject){
+        this.isnew=isnew;
+        this.obj=obj;
+    }
+}
+#if nape_swc@:keep #end
+class TObject{
+    public static var nextId=0;
+    public var id:Int;
+    public var obj:Dynamic;
+    public var type:String;
+    public var references:Array<Reference>;
+    public function new(obj:Dynamic){
+        id=nextId++;
+        this.obj=obj;
+        type=Type.getClassName(Type.getClass(obj));
+        references=new Array<Reference>();
+    }
+    public function toString(){
+        var ret="#"+id+" : "+type+";\n";
+        for(r in references)ret+="\t "+r.name+" = #"+r.obj.id+"\n";
+        return ret.substr(0,ret.length-1);
+    }
+}
+#if haxe3 typedef IntHash<T>=Map<Int,T>;
+#end
+#if nape_swc@:keep #end
+class Graph{
+    public var objects:IntHash<TObject>;
+    var dict:flash.utils.Dictionary;
+    public function new(objs:Array<Dynamic>=null){
+        objects=new IntHash<TObject>();
+        dict=new flash.utils.Dictionary(false);
+        if(objs==null)return;
+        for(o in objs)push(o);
+    }
+    public function push(obj:Dynamic):PushResult{
+        if(Reflect.isObject(obj)){
+            if((untyped dict[obj])==null){
+                var ob=new TObject(obj);
+                untyped dict[obj]=ob;
+                objects.set(ob.id,ob);
+                return new PushResult(true,ob);
+            }
+            else return new PushResult(false,untyped dict[obj]);
+        }
+        else return null;
+    }
+    public function toString(){
+        var ret="";
+        for(o in objects)ret+=o.toString()+"\n";
+        return ret;
+    }
+    public function populate(continuation:Void->Void){
+        var stack:Array<Dynamic>=[];
+        for(o in objects){
+            o.references=[];
+            stack.push(o);
+        }
+        var tim=new haxe.Timer(0);
+        tim.run=function(){
+            if(stack.length==0){
+                tim.stop();
+                continuation();
+                return;
+            }
+            var pt=flash.Lib.getTimer();
+            while(flash.Lib.getTimer()-pt<50){
+                if(stack.length==0){
+                    tim.stop();
+                    continuation();
+                    return;
+                }
+                var obj:TObject=stack.shift();
+                var cls=Type.resolveClass(obj.type);
+                for(o in Type.getInstanceFields(cls)){
+                    var field:Dynamic=Reflect.field(obj.obj,o);
+                    if(field!=null&&Reflect.isObject(field)){
+                        var ref=push(field);
+                        obj.references.push(new Reference(o,ref.obj));
+                        if(ref.isnew)stack.push(ref.obj);
+                    }
+                }
+            }
+        }
+    }
+}
